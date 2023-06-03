ca2919fa4487acdd0b746a607a38abd023ed1867
Ericsen Tsai <bommer95175@gmail.com>
2023-06-04 01:14:18 +0800

    feat: add page for titans

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+    react(),
   ],
 });
