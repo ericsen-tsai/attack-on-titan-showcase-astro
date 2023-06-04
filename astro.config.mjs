75d0975eb31402dd43a79ada84ac668bd05cc26a
Ericsen Tsai <bommer95175@gmail.com>
2023-06-04 10:08:33 +0800

    build: add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import react from "@astrojs/react";
+
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +14,6 @@ export default defineConfig({
     }),
     react(),
   ],
+  output: "server",
+  adapter: vercelStatic(),
 });
